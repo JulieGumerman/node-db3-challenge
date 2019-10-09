021d65cbf36cc17a850a41a5f5feae10e4cfe921
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-09 15:34:02 -0600

    so clooose to mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -14,30 +14,29 @@ const findById = (id) => {
 }
 
 const findSteps = (id) => {
-//   - Expects a scheme `id`.
-//   - Resolves to an array of all correctly ordered step for the given scheme: `[ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ]`.
-//   - This array should include the `scheme_name` _not_ the `scheme_id`.    
-    return db("steps").where({id})
+
+    return db("steps")
+        .join("schemes", "steps.scheme_id", "schemes.id", )
+        .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+        .where({scheme_id: id})
+        .orderBy("steps.step_number")
 
 }
 
 const add = (scheme) => {
-//   - Expects a scheme object.
-//   - Inserts scheme into the database.
-//   - Resolves to the newly inserted scheme, including `id`.    
+    return db("schemes").insert(scheme);
+ 
 }
 
 const update = (changes, id) => {
-//   - Expects a changes object and an `id`.
-//   - Updates the scheme with the given id.
-//   - Resolves to the newly updated scheme object.    
+
+    return db("schemes").where({id}).update(changes);
+
 }
 
 const remove = (id) => {
-//   - Removes the scheme object with the provided id.
-//   - Resolves to the removed scheme
-//   - Resolves to `null` on an invalid id.
-//   - (Hint: Only worry about removing the `scheme`. The database is configured to automatically remove all associated steps.)    
+
+    return db("schemes").where({id}).delete();
 }
 
 module.exports = {
